6573c5d8874283867c0cc3bf28fdcac7d6a6cde9
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-05-30 14:33:59 -0300

    posts

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     },
     title: {
         color: '#000',
-        fontFamily: 'shelter',
+        fontFamily: 'TravelingTypewriter',
         height: 30,
         fontSize: 28
     }
