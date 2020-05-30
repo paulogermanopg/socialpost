667f8824f37716bd124781efa680d575b12b0635
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-05-30 18:56:21 -0300

    comments

--- App.js
+++ App.js
@@ -5,10 +5,19 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+    const comments = [{
+      nickname: 'Benner Connor Temmaori',
+      comment: 'Aí é perto de meu esconderijo'
+    }, {
+      nickname: 'Kayo C. Lonk',
+      comment: 'KKKKK. Longe dos olhos do Novo Governo, sei'
+    }]
+
     return(
       <View style={{ flex: 1 }}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} 
+          comments={comments}/>
       </View>
       
     )
